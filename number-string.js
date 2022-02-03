3f1c67c1bd1c39554da6d97b5d5c5f5fe6ee8c8e
Tilek <askaralievtilek@gmail.com>
2022-02-03 21:02:44 +0600

    number h/w

--- number-string.js
+++ number-string.js
@@ -10,17 +10,21 @@
 // //1
 // let num = 5;
 // console.log(num);
+
 // //2
 // let a = 10;
 // let b = 2;
 // let c = 5;
 // console.log(a + b + c);
+
 // // 3
 // let number = 100;
 // console.log(number * 2);
+
 // // 4
 // let num = 67;
 // console.log(num % 7);
+
 // // 5
 // let a = 10;
 // let b = 2;
@@ -28,16 +32,19 @@
 // console.log(a - b);
 // console.log(a * b);
 // console.log(a / b);
+
 // // 6
 // let c = 15;
 // let d = 2;
 // let result = c + d;
 // console.log(result);
+
 // // 7
 // let a = 30;
 // let b = 15;
 // let c = a + b;
 // console.log(c % 4);
+
 // // 8
 // let a = 17;
 // let b = 10;
@@ -45,11 +52,13 @@
 // let d = c;
 // let result = c + d;
 // console.log(result);
+
 // // 9
 // let a = '10';
 // let b = '99';
 // let result = a + b;
 // console.log(result);
+
 // // 10
 // let name = 'лет';
 // let number = 10;
