af423a8b6bbfa4bf576dd0990b9240ab476fe366
Tilek <askaralievtilek@gmail.com>
2022-02-03 19:18:07 +0600

    home-work1

--- main.js
+++ main.js
@@ -1,8 +1,59 @@
-//var 
-//let
-//const
+// //var 
+// //let
+// //const
 
-let name = 50; //string
-let age = 50; //number
+// let name = 50; //string
+// let age = 50; //number
 
-console.log(name >= age);
+// console.log(name >= age);
+
+// //1
+// let num = 5;
+// console.log(num);
+// //2
+// let a = 10;
+// let b = 2;
+// let c = 5;
+// console.log(a + b + c);
+// // 3
+// let number = 100;
+// console.log(number * 2);
+// // 4
+// let num = 67;
+// console.log(num % 7);
+// // 5
+// let a = 10;
+// let b = 2;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// // 6
+// let c = 15;
+// let d = 2;
+// let result = c + d;
+// console.log(result);
+// // 7
+// let a = 30;
+// let b = 15;
+// let c = a + b;
+// console.log(c % 4);
+// // 8
+// let a = 17;
+// let b = 10;
+// let c = a - b;
+// let d = c;
+// let result = c + d;
+// console.log(result);
+// // 9
+// let a = '10';
+// let b = '99';
+// let result = a + b;
+// console.log(result);
+// // 10
+// let name = 'лет';
+// let number = 10;
+// result = name + number;
+// console.log(result);
+
+
