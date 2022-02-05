0a357ee5b1d52ae4b81855c350cfd1e1242825ad
Tilek <askaralievtilek@gmail.com>
2022-02-05 19:20:36 +0600

    objects-array h/w

---
+++ object-array.js
@@ -0,0 +1,84 @@
+//1
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// };
+// let sum = salaries.John + salaries.Ann + salaries.Pete;
+// console.log(sum)
+
+// 2
+// let obj = {
+//     name: 'Amir',
+//     age: 15
+// };
+// console.log(obj.name);
+
+// 3
+// let obj = {
+//     name:"Amir",
+//     age:15
+// };
+// obj.age += 5;
+// console.log(obj.age)
+
+// 4
+// let obj = {
+//     name:'Amir',
+//     student: true
+// };
+// obj.student = !obj.student;
+// console.log(obj.student);
+
+// 5
+// let obj = {
+//     name:"ABRACADABRA",
+//     age: 15,
+//     student: true
+// };
+// console.log(obj.name.includes('BRAC'));
+
+// 6
+// let obj = {
+//     name:'Amir',
+//     age: 15,
+//     student: true
+// };
+// console.log(Object.values(obj));
+// console.log(Object.keys(obj));
+// console.log(Object.entries(obj));
+
+// 7
+// let obj = {
+//     name:'Amir',
+//     age: 15,
+//     student: true
+// };
+// let obj2 = JSON.parse(JSON.stringify(obj))
+// obj2.student = !obj2.student;
+//
+// console.log(obj)
+// console.log(obj2);
+
+// 8
+// let cars =  ['bmw', 'honda','mers','lexus'];
+// cars.pop();
+// console.log(cars)
+
+// 9
+// let cars =  ['bmw', 'honda','mers','lexus'];
+// console.log(cars[cars.length - 1]);
+
+// 10
+// let cars = ['bmw', 'honda','mers','lexus'];
+// cars[2] = 'toyota';
+// console.log(cars)
+
+// 11
+// let cars = ['bmw', 'honda','mers','lexus'];
+// cars.unshift('kia');
+// console.log(cars)
+
+// 12
+// let cars = ['bmw', 'honda','mers','lexus'];
+// console.log(cars.length);
