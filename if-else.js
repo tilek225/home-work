d6c58fa8fd765ef17538758fdeb1e2523b3336c0
Tilek <askaralievtilek@gmail.com>
2022-02-08 21:31:26 +0600

    if-else

---
+++ if-else.js
@@ -0,0 +1,27 @@
+// let min = 48;
+//
+// if(min >= 0 && min <= 14){
+//     console.log('В первую четверть')
+// } else if(min >= 15 && min <= 30){
+//     console.log('В вторую четверть')
+// } else if(min >= 31 && min <= 45){
+//     console.log('В третью четверть')
+// } else if(min >= 46 && min <= 59){
+//     console.log('В четвертую четверть')
+// }
+
+// let arr = ['xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl'];
+//
+// if(arr.includes('xs' || 's' || 'm' || 'l')){
+//     console.log('small')
+// }else if(arr.includes('xl')){
+//     console.log('middle')
+// }else if(arr.includes('xxl' || 'xxxl')){
+//     console.log('big')
+// }
+
+// let a = '2';
+// let o = '3'
+// console.log(+a + o)
+
+// console.log(3 && 5 || 8 && 0) // 5
