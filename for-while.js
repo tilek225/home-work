cb75dc7e3a30e13ee7d7f33323bae8f62cd71288
Tilek <askaralievtilek@gmail.com>
2022-02-16 17:34:54 +0600

    for-while h-w

---
+++ for-while.js
@@ -0,0 +1,100 @@
+// 1
+// let arr = [1, 2, 3, 4, 5];
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// 2
+// let result = 0;
+// let arr = [1, 2, 3, 4, 5];
+// for(let i = 0; i < arr.length; i++){
+//     result+=arr[i];
+// }
+// console.log(result);
+
+// 3
+// let i = 0;
+// while (i < 3){
+//     console.log(`number ${i}`);
+//     i++;
+// }
+
+// 4
+// let arr = [-2,7,9,5,8,1,12,15,17,3];
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] < 0){
+//         continue;
+//     }
+//     else if(arr[i] >= 10){
+//         continue;
+//     }
+//     console.log(arr[i])
+// }
+
+// 5
+// for(let i = 2; i <=10; i+=2){
+//     console.log(i)
+// }
+
+// 6
+// let result = 0;
+// for(let i = 1; i < 100; i++){
+//     result+=i;
+// }
+// console.log(result)
+
+// 7
+// let cars = ['BMW', 'Mersedes', 'Opel', 'Mazda', 'FIAT', 'Ferarri', 'Reno'];
+// let result;
+// for(let i = 0; i < cars.length; i++){
+//     if(cars[i] === cars[i].toUpperCase()){
+//         result = cars[i]
+//         console.log(result)
+//     }
+// }
+
+// 8
+// let arr = [2, 3, 4, 5, 6, 7];
+// let arr1;
+// for(let i = 0; i < arr.length; i++){
+//   arr[i] = arr[i] * 2;
+// }
+// console.log(arr)
+
+// 9
+// let arr = [-2, 7, '9б', 'Vlad', 1, 12, 15, 'Ivan', 17, 3];
+// for(let i = 0; i<arr.length; i++){
+//     if(arr[i] === +arr[i]){
+//         continue;
+//     }
+//     console.log(arr[i])
+// }
+
+// 10
+// let arr = ['Понедельник', 'Вторник', 'Cреда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+// for(let i = 0; i < arr.length; i++){
+//    if(arr[i] === arr[5] || arr[i] === arr[6]){
+//        arr[i] = 'Выходной'
+//    }
+// }
+// console.log(arr);
+
+// 11
+// let arr = ['Наташа','Анастасия','Дмитрий','Максим'];
+// for(let i = 1; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// 12
+// let arr = ['Наташа', 'Анастасия', 2, 'Максим', 4];
+// for (let i = 0; i < arr.length; i++){
+//     arr.sort();
+// }
+// console.log(arr)
+
+// 13
+// let result = 0;
+// for(let i = 2; i <=10; i++){
+//     result = i * 5;
+//     console.log(result)
+// }
