c14828675f4ba4c9540c4af7fb0cb9d6a2d36493
Tilek <askaralievtilek@gmail.com>
2022-03-09 17:31:58 +0600

    dom h-w

---
+++ dom.js
@@ -0,0 +1,38 @@
+// 1
+// let title = document.querySelectorAll('.title')
+// console.log(title);
+
+// 2
+// let title = document.querySelector('.title');
+// console.log(title);
+
+// 3
+// let title = document.querySelectorAll('.title');
+// console.log(Array.from(title));
+
+// 4
+// let title = document.querySelector('p')
+// console.log(title);
+
+// 5
+// let circle = document.querySelector('.circle');
+// circle.style.cssText = 'width: 200px; height: 200px; background: purple; border-radius: 50%'
+
+// 6
+// let text = document.querySelector('.text');
+// text.setAttribute('id', 'succsess')
+// console.log(text);
+
+// 7
+// let li = document.querySelectorAll('li');
+// li = Array.from(li);
+// li.forEach((item) => {
+//     item.textContent = 'Заменилось'
+// })
+
+
+// 8
+// let li = document.querySelectorAll('li');
+// li.forEach((item) => {
+//     item.style.cssText = 'background: red'
+// })
